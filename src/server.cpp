4e19b8356bfeb8fc7ca406b60364dbe25877f1da
lzq <lzq47@foxmail.com>
2025-07-30 00:27:53 +0800

    use libuv

--- src/server.cpp
+++ src/server.cpp
@@ -9,6 +9,8 @@
 #include <utility>
 #include <vector>
 
+#include <uv.h>
+
 #include "channel.h"
 #include "task.h"
 
@@ -17,32 +19,45 @@ namespace mygo {
 const int MAX_THREAD = 1;
 
 class Loop {
- public:
+public:
   using Chan = Channel<std::unique_ptr<Task>>;
   Loop(std::string_view id) : id_(id) {
     pending_tasks_ = std::make_unique<Chan>();
+    uv_loop_ = uv_default_loop();
   }
-  Loop(Loop&& moved) {
+  Loop(Loop &&moved) {
     pending_tasks_ = std::move(moved.pending_tasks_);
     moved.pending_tasks_ = nullptr;
     id_ = moved.id_;
     finished_ = false;
+    uv_loop_ = moved.uv_loop_;
+    moved.uv_loop_ = nullptr;
   }
+
+  ~Loop() { uv_loop_close(uv_loop_); }
 
   void PostTask(std::unique_ptr<Task> t) { pending_tasks_->Push(std::move(t)); }
 
-  void PostTask(std::function<void(Loop*)> func) {
+  void PostTask(std::function<void(Loop *)> func) {
     pending_tasks_->Push(std::unique_ptr<Task>(new RunOnce(func)));
   }
 
-  void PostTimerTask(bool repeat, std::function<void(Loop*)> func, Duration d) {
+  void PostTimerTask(bool repeat, std::function<void(Loop *)> func,
+                     Duration d) {
     pending_tasks_->Push(std::unique_ptr<Task>(new TimerTask(repeat, func, d)));
   }
 
   std::string id() { return id_; }
   void run() {
     std::cout << id_ << " started" << std::endl;
+    uv_timer_t t;
+    uv_timer_init(uv_loop_, &t);
+    uv_timer_start(
+        &t, [](uv_timer_t *) { std::cout << "timer timeout" << std::endl; },
+        100, 100);
+
     while (pending_tasks_->Size() > 0) {
+      uv_run(uv_loop_, UV_RUN_ONCE);
       std::unique_ptr<Task> task = pending_tasks_->Pop();
       Task::State state = task->run(this);
       if (state == Task::State::NotFinish) {
@@ -52,10 +67,11 @@ class Loop {
     finished_ = true;
   }
 
- private:
+private:
   std::unique_ptr<Chan> pending_tasks_;
   std::string id_;
   bool finished_ = false;
+  uv_loop_t *uv_loop_ = nullptr;
 };
 
 int Server::Run() {
@@ -69,22 +85,24 @@ int Server::Run() {
   }
 
   int i = 0;
-  for (auto& loop : loops) {
+  for (auto &loop : loops) {
     std::string id = loop.id();
-    loop.PostTask([i, id](Loop* this_loop) {
+    loop.PostTask([i, id](Loop *this_loop) {
       std::this_thread::sleep_for(Duration(1000));
       std::cout << id << " " << i << std::endl;
       this_loop->PostTimerTask(
           true,
-          [i](Loop*) { std::cout << "its a callback after" << i << std::endl; },
+          [i](Loop *) {
+            std::cout << "its a callback after" << i << std::endl;
+          },
           Duration((i + 1) * 1000));
     });
     i++;
 
-    threads.emplace_back([](Loop* loop) { loop->run(); }, &loop);
+    threads.emplace_back([](Loop *loop) { loop->run(); }, &loop);
   }
 
-  for (std::thread& thread : threads) {
+  for (std::thread &thread : threads) {
     thread.join();
   }
 
@@ -97,4 +115,4 @@ int Server::Stop() {
   return 0;
 }
 
-}  // namespace mygo
+} // namespace mygo
